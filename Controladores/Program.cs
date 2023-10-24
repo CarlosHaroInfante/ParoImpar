f908c3548a1b5ab87a81e3d6b44c39a1a698f158
DESKTOP-8FK18PG\Carlos <csi23-charnte@colegioaltair.net>
2023-10-24 11:21:58 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ParoImpar
+{
+
+    using ParoImpar.Servicios;
+    using System.ComponentModel.Design;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MenuIntefaz num = new MenuImplementacion();
+
+            int numero = num.numero();
+
+            if ((numero % 2) == 0)
+            {
+                Console.WriteLine("Es par");
+            }
+            else
+            {
+                Console.WriteLine("Es Impar");
+            }
+        }
+
+
+    }
+}
---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParoImpar.Servicios
+{
+    internal class MenuImplementacion : MenuIntefaz
+    { 
+
+
+        public int numero()
+        {
+            int numero;
+            Console.WriteLine("Elige un número");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numero); 
+            return numero;
+        }
+    }
+}
